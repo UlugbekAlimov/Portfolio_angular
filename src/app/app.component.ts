01342c692bbbf6a6b57572899019c84ab4d5b387
Alimov Ulugbek <alimovu966@gmail.com>
2024-11-28 14:13:03 +0500

    home-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent, HomeComponent],
+  imports: [RouterOutlet , NavbarComponent, HomeComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+  isSplashVisible = true;
+  textClass = 'focus-in-expand';
+  splashScreenClass = '';
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.textClass = 'blur-out-contract';
+    }, 2000); 
+
+    setTimeout(() => {
+      this.splashScreenClass = 'fade-out';
+    }, 3000); 
+
+    setTimeout(() => {
+      this.isSplashVisible = false;
+    }, 3500);  
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isMenuVisible: boolean = false;
 
+  ngOnInit(): void {
+    this.updateMenuVisibility();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.updateMenuVisibility();
+  }
+
+  private updateMenuVisibility() {
+    const screenWidth = window.innerWidth;
+    this.isMenuVisible = screenWidth >= 768;
+  }
 }
